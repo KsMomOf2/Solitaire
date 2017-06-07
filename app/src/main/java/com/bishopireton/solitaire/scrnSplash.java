957c3dab35857d496cc79422edc697fd48bbf28e
innisa <innisa@bishopireton.org>
2017-06-07 17:09:10 -0400

    6/7 create a "splash screen"

--- app/src/main/java/com/bishopireton/solitaire/scrnSplash.java
+++ app/src/main/java/com/bishopireton/solitaire/scrnSplash.java
@@ -1,7 +1,10 @@
 package com.bishopireton.solitaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class scrnSplash extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class scrnSplash extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_scrn_splash);
+
+    Button btnStart= (Button) findViewById(R.id.btnStart);
+    btnStart.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View v) {
+        startActivity(new Intent(scrnSplash.this,scrnGame.class));
+      }
+    });
   }
 }
